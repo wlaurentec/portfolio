d98b0716c45b5ae07298548cf5e600857cd0812e
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-25 08:30:08 -0500

    create navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import NavBar from "./Components/NavBar/NavBar"
 
 const App = () => {
   return (
     <div>
-      
+      <NavBar />
     </div>
   )
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import "./NavBar.css"
+import React from "react";
+import "./NavBar.css";
+import logo from "../../assets/logo.svg";
 
 const NavBar = () => {
   return (
-    <div className='NavBar'>
-      
+    <div className="NavBar">
+      <img src={logo} alt="" />
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect with me</div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;

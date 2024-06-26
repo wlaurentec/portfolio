f6faa4ca3b380b33d9b60fbfdce3529cd491c26e
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-26 11:39:04 -0500

    add mywork component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./Components/About/About"
 import Hero from "./Components/Hero/Hero"
 import NavBar from "./Components/NavBar/NavBar"
 import Services from "./Components/Services/Services"
+import MyWork from "./Components/MyWork/MyWork"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <About />
       <Services />
+      <MyWork />
     </div>
   )
 }
---
+++ src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,25 @@
+import "./MyWork.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import mywork_data from "../../assets/mywork_data";
+import arrow_icon from "../../assets/arrow_icon.svg";
+const MyWork = () => {
+  return (
+    <div className="mywork">
+      <div className="mywork-title">
+        <h1>My latest work</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="mywork-container">
+        {mywork_data.map((work, index) => {
+          return <img key={index} src={work.w_img} alt="" />;
+        })}
+      </div>
+      <div className="mywork-showmore">
+        <p>Show more</p>
+        <img src={arrow_icon} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default MyWork;
